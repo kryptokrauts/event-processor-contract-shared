dcdfc5e763057e27c017c18e7cc9dc719c7c7fe0
Michel Meier <dna.fan@gmx.de>
2024-05-10 12:35:05 +0200

    feat: add retry exception class

---
+++ src/main/java/com/kryptokrauts/shared/contract/exception/RetryException.java
@@ -0,0 +1,12 @@
+package com.kryptokrauts.sync.exception;
+
+public class RetryException extends RuntimeException {
+
+  public RetryException(String msg, Exception e) {
+    super(msg, e);
+  }
+
+  public RetryException(Exception e) {
+    super(e);
+  }
+}
