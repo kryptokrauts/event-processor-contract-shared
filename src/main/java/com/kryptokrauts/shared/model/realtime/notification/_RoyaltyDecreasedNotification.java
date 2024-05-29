98b303639cda62ca44100632ce2eae7c3687131d
Michel Meier <dna.fan@gmx.de>
2024-05-29 16:56:37 +0200

    chore: remove duplicate creator field from decreased notification

--- src/main/java/com/kryptokrauts/shared/model/realtime/notification/_RoyaltyDecreasedNotification.java
+++ src/main/java/com/kryptokrauts/shared/model/realtime/notification/_RoyaltyDecreasedNotification.java
@@ -1,6 +1,5 @@
 package com.kryptokrauts.shared.model.realtime.notification;
 
-import com.kryptokrauts.shared.model.common._Account;
 import com.kryptokrauts.shared.model.realtime._Notification;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Getter;
@@ -14,6 +13,4 @@ public class _RoyaltyDecreasedNotification extends _Notification {
   private Double oldValue;
 
   private Double newValue;
-
-  private _Account creator;
 }
