4f15bbee3a23f0cf110e12b5a704114a07a8246e
Michel Meier <dna.fan@gmx.de>
2024-07-03 09:43:05 +0200

    refactoring-1: move contract classes to new project

--- src/test/java/com/kryptokrauts/shared/entity/EventLogEntity.java
+++ src/test/java/com/kryptokrauts/shared/entity/EventLogEntity.java
@@ -1,17 +1,11 @@
 package com.kryptokrauts.shared.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
 public class EventLogEntity extends PanacheEntityBase {
-
-  @Id private long id;
-
-  private long blocknum;
 
   private String type;
 
