3a430e47dea0a11cc194043ce0afab028d7f398f
Michel Meier <dna.fan@gmx.de>
2024-07-11 14:06:01 +0200

    chore: refactoring to profile base view

--- src/main/java/com/kryptokrauts/shared/dao/common/ProfileBaseView.java
+++ src/main/java/com/kryptokrauts/shared/dao/common/ProfileBaseView.java
@@ -1,15 +1,14 @@
 package com.kryptokrauts.shared.dao.common;
 
 import com.kryptokrauts.shared.model.common._Account;
-
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
 
 @Getter
-@Entity(name = "soonmarket_profile")
-public class ProfileBaseEntity extends PanacheEntityBase {
+@Entity(name = "soonmarket_profile_base_v")
+public class ProfileBaseView extends PanacheEntityBase {
   @Id private String account;
 
   private Boolean hasKyc;
